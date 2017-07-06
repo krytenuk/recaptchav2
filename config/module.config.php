4f0915a52ad6090f22cb033bf482bc0f2bc96f7f
krytenuk <info@freedomwebservices.net>
2017-07-06 19:54:24 +0100

    Changed loading of Zend Form Element to service manager

--- config/module.config.php
+++ config/module.config.php
@@ -16,8 +16,8 @@ return [
             'formFwsReCaptchaV2' => 'FwsReCaptchaV2\Form\View\Helper\FormReCaptchaV2',
         ],
         'factories' => [
-            'formElement' => function($sm) {
-                $helper = new \Zend\Form\View\Helper\FormElement();
+            'formElement' => function($serviceManager) {
+                $helper = $serviceManager->get('Zend\Form\View\Helper\FormElement');
                 $helper->addClass('FwsReCaptchaV2\Form\Element\ReCaptchaV2', 'formFwsReCaptchaV2');
                 return $helper;
             }
