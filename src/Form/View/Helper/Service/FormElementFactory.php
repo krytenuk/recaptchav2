1f2a4c01c61e555c3ffea048d4768e7af577b49e
Garry Childs <info@freedomwebservices.net>
2018-08-29 13:46:03 +0100

    updated formelementfactory

---
+++ src/Form/View/Helper/Service/FormElementFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace FwsReCaptchaV2\Form\View\Helper\Service;
+
+use Zend\ServiceManager\Factory\FactoryInterface;
+use Interop\Container\ContainerInterface;
+use FwsReCaptchaV2\Form\Element\ReCaptchaV2 as ReCaptchaV2FormElement;
+use FwsReCaptchaV2\Form\View\Helper\FormReCaptchaV2;
+use Zend\Form\View\Helper\FormElement;
+
+/**
+ * FormElementFactory
+ *
+ * @author Garry Childs (info@freedomwebservices.net)
+ */
+class FormElementFactory implements FactoryInterface
+{
+
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
+    {
+        $helper = new FormElement();
+        $helper->addClass(ReCaptchaV2FormElement::class, FormReCaptchaV2::class);
+        return $helper;
+    }
+
+}
